8247313c77373ae69b98e8265ea5d121b609ec9d
YokoyamaSeimei <unagi1017hiki@yahoo.co.jp>
2022-07-14 05:12:46 +0900

    HoT: initial revision

---
+++ HeadTail.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+public class HeadTail{
+    public static void main(String[] args) {
+        int count=0;
+        int count2=0;
+        System.out.printf("Tossing a coin...\n");
+        for(int i=1;i<4;i++){
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(11);
+        int headtail = randomNumber%2;
+        if(headtail==0){
+        System.out.printf("Round %d : Heads\n",i);
+        count++;}
+        else {System.out.printf("Round %d : Tails\n",i);
+        count2++;
+        }
+        }
+        System.out.printf("Heads: %d , Tails: %d\n",count,count2);
+    }
+}
